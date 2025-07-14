4042d0714cf893aed039999b69d35a53827f0016
Alexandros <alexandros.kazalis@hotmail.com>
2025-07-14 05:41:58 +0200

    First Commit

---
+++ src/main/java/com/alexandros/dailycompanion/CatholicDailyCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.alexandros.dailycompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatholicDailyCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatholicDailyCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandros/dailycompanion/CatholicDailyCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandros.dailycompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatholicDailyCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
