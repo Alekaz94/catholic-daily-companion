89219cdb61cb28d7c4db175d9b803c57e9f48c57
alexandroskazalis <alexandros.kazalis@hotmail.com>
2025-09-03 21:00:04 +0200

    Added unique constraint on email for usermodel

--- src/main/java/com/alexandros/dailycompanion/model/User.java
+++ src/main/java/com/alexandros/dailycompanion/model/User.java
@@ -26,6 +26,7 @@ public class User {
     private UUID id;
     private String firstName;
     private String lastName;
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
     private LocalDate createdAt;
