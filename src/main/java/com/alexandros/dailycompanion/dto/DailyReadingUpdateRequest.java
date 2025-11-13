93852a7432ea4be397f51beff24e32633442cffa
alexandroskazalis <alexandros.kazalis@hotmail.com>
2025-11-13 22:17:19 +0100

    Removed unused files for daily readings.

--- src/main/java/com/alexandros/dailycompanion/dto/DailyReadingUpdateRequest.java
+++
@@ -1,7 +0,0 @@
-package com.alexandros.dailycompanion.dto;
-
-public record DailyReadingUpdateRequest(String firstReading,
-                                        String secondReading,
-                                        String psalm,
-                                        String gospel) {
-}
