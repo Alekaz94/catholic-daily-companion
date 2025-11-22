06b046485bdd0602938e7337cf250caadd3914c7
alexandroskazalis <alexandros.kazalis@hotmail.com>
2025-11-22 10:54:11 +0100

    - Optional license header added to feedbackRequest to indicate copyright
    also added boolean isFixed
    
    added Terms of Service html file to project

--- src/main/java/com/alexandros/dailycompanion/dto/FeedbackRequest.java
+++ src/main/java/com/alexandros/dailycompanion/dto/FeedbackRequest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 Alexandros Kazalis
  *
  * Licensed under the MIT License. See LICENSE file in the project root for details.
- */
+*/
 
 package com.alexandros.dailycompanion.dto;
 
