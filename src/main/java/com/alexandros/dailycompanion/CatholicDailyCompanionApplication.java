89553dcb32720841065165ee1c9f352ee8c2a422
alexandroskazalis <alexandros.kazalis@hotmail.com>
2025-11-24 23:36:54 +0100

    Removed the default admin that is created at start

--- src/main/java/com/alexandros/dailycompanion/CatholicDailyCompanionApplication.java
+++ src/main/java/com/alexandros/dailycompanion/CatholicDailyCompanionApplication.java
@@ -29,27 +29,6 @@ public class CatholicDailyCompanionApplication {
 
 	@Bean
 	@Order(1)
-	CommandLineRunner initUsers(UserRepository userRepository,
-								@Value("${admin.default.email}") String adminEmail,
-								@Value("${admin.default.password:changeMe123}") String adminPassword) {
-		return args -> {
-			if (userRepository.findByEmail(adminEmail).isEmpty()) {
-				User adminUser = new User();
-				adminUser.setFirstName("Admin");
-				adminUser.setLastName("User");
-				adminUser.setEmail(adminEmail);
-				adminUser.setPassword(PasswordUtil.hashPassword(adminPassword));
-				adminUser.setRole(Roles.ADMIN);
-				userRepository.save(adminUser);
-				System.out.println("Admin user created: " + adminEmail);
-			} else {
-				System.out.println("Admin user already exists: " + adminEmail);
-			}
-		};
-	}
-
-	@Bean
-	@Order(2)
 	public CommandLineRunner seedData(DataSeeder dataSeeder) {
 		return args -> {
 			dataSeeder.seedSaintsIfEmpty();
